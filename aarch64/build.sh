cde8d969a1b7fc57e663481e68b8570c8eba5359
thealhu <huthanslhd@gmail.com>
2023-03-08 01:10:14 +0100

    Add aarch64

---
+++ aarch64/build.sh
@@ -0,0 +1,16 @@
+rm -rf docker-tvheadend
+git clone https://github.com/linuxserver/docker-tvheadend.git
+mkdir docker-tvheadend/patches/argtable
+mkdir docker-tvheadend/patches/cpan
+mkdir docker-tvheadend/patches/libdvbcsa
+mkdir docker-tvheadend/patches/tvheadend
+mv docker-tvheadend/patches/config.guess docker-tvheadend/patches/argtable/
+mv docker-tvheadend/patches/config.sub docker-tvheadend/patches/argtable/
+mv docker-tvheadend/patches/cpanfile docker-tvheadend/patches/cpan
+cp files/libdvbcsa.patch docker-tvheadend/patches/libdvbcsa/
+cp files/tvheadend43.patch docker-tvheadend/patches/tvheadend/
+cp files/cpanm docker-tvheadend/
+cp files/10-adduser docker-tvheadend/
+cat files/docker > docker-tvheadend/Dockerfile
+cd docker-tvheadend/
+docker build -t thealhu/tvheadend-aarch64 --network host --no-cache .
