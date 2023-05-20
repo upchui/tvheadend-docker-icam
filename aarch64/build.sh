65662e51c7e23567ffc099aeff6e664d63c3561d
alhu <alexander.huthansl@axians.at>
2023-05-20 20:04:59 +0200

    removed cpan and now uses xmltv from repository

--- aarch64/build.sh
+++ aarch64/build.sh
@@ -1,12 +1,10 @@
 rm -rf docker-tvheadend
 git clone https://github.com/linuxserver/docker-tvheadend.git
 mkdir docker-tvheadend/patches/argtable
-mkdir docker-tvheadend/patches/cpan
 mkdir docker-tvheadend/patches/libdvbcsa
 mkdir docker-tvheadend/patches/tvheadend
 mv docker-tvheadend/patches/config.guess docker-tvheadend/patches/argtable/
 mv docker-tvheadend/patches/config.sub docker-tvheadend/patches/argtable/
-mv docker-tvheadend/patches/cpanfile docker-tvheadend/patches/cpan
 cp files/libdvbcsa.patch docker-tvheadend/patches/libdvbcsa/
 cp files/tvheadend43.patch docker-tvheadend/patches/tvheadend/
 cp files/cpanm docker-tvheadend/
