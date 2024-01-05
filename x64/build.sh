f5c9c01bab1633f9985ac3c7ef009b812042ca80
alhu <me@alhu.at>
2024-01-05 02:41:35 +0100

    test suff

--- x64/build.sh
+++ x64/build.sh
@@ -3,8 +3,8 @@ git clone https://github.com/linuxserver/docker-tvheadend.git
 #mkdir docker-tvheadend/patches/argtable
 #mkdir docker-tvheadend/patches/libdvbcsa
 #mkdir docker-tvheadend/patches/tvheadend
-mv docker-tvheadend/patches/config.guess docker-tvheadend/patches/argtable/
-mv docker-tvheadend/patches/config.sub docker-tvheadend/patches/argtable/
+#mv docker-tvheadend/patches/config.guess docker-tvheadend/patches/argtable/
+#mv docker-tvheadend/patches/config.sub docker-tvheadend/patches/argtable/
 #cp files/libdvbcsa.patch docker-tvheadend/patches/libdvbcsa/
 #cp files/tvheadend43.patch docker-tvheadend/patches/tvheadend/
 cp files/10-adduser docker-tvheadend/
