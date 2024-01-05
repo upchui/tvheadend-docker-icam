ee59e317f34e0437730eed1704a28af978de5f1a
alhu <me@alhu.at>
2024-01-05 02:44:32 +0100

    test suff

--- x64/build.sh
+++ x64/build.sh
@@ -10,7 +10,7 @@ git clone https://github.com/linuxserver/docker-tvheadend.git
 cp files/10-adduser docker-tvheadend/
 #rm -rf docker-tvheadend/src/descrambler/caid.h docker-tvheadend/src/descrambler/descrambler.c docker-tvheadend/src/descrambler/tvhcsa.c docker-tvheadend/src/descrambler/tvhcsa.h
 #mv files/fix_for_899b38ae5b960688b600be3e77526d92cecea536/* docker-tvheadend/src/descrambler/
-cat files/docker > docker-tvheadend/Dockerfile
+#cat files/docker > docker-tvheadend/Dockerfile
 cat files/metric_heartbeat.sh > docker-tvheadend/metric_heartbeat.sh
 cd docker-tvheadend/
 docker build -t thealhu/tvheadend-x64 --network host --no-cache .
